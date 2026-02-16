01fc5e01e15682cf716576423edd0eabdedfed5b
Ign1s123 <genamisinskij@gmail.com>
2026-02-16 21:15:11 +0200

    fixed task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,34 +1,17 @@
-const titleCategories = document.querySelectorAll(`.js-title-categories`);
 const item = document.querySelectorAll(`.item`);
-const listPointsAnimals = document.querySelectorAll(`.list-points-animal`);
-const listPointsProducts = document.querySelectorAll(`.list-points-products`);
-const listPointsTechnologies = document.querySelectorAll(`.list-points-technologies`)
 
 
 let titleArr = [];
-let liAnimalsArr = [];
-let liProductsArr = [];
-let liTechnologiesArr = [];
+let liArr = [];
 
-
-item.forEach((li) => {
+item.forEach(li => {
   const title = li.querySelector('h2');
   titleArr.push(title)
 });
 
-listPointsAnimals.forEach((ul) => {
-    const itemPoints = ul.querySelectorAll(`li`);
-    liAnimalsArr.push(...itemPoints);
-})
-
-listPointsProducts.forEach((ul) => {
-    const itemPoints = ul.querySelectorAll(`li`);
-    liProductsArr.push(...itemPoints);
-})
-
-listPointsTechnologies.forEach((ul) => {
-    const itemPoints = ul.querySelectorAll(`li`);
-    liTechnologiesArr.push(...itemPoints);
+item.forEach(li => {
+    const items = li.querySelectorAll(`li`);
+    liArr.push(items);
 })
 
 
@@ -38,10 +21,12 @@ listPointsTechnologies.forEach((ul) => {
 
 
 
-console.log(`Number of categories: ${titleCategories.length}`);
+
+
+console.log(`Number of categories: ${item.length}`);
 console.log(`Category: ${titleArr[0].textContent}`);
-console.log(`Elements: ${liAnimalsArr.length}`);
+console.log(`Elements: ${liArr[0].length}`);
 console.log(`Category: ${titleArr[1].textContent}`);
-console.log(`Elements: ${liProductsArr.length}`);
+console.log(`Elements: ${liArr[1].length}`);
 console.log(`Category: ${titleArr[2].textContent}`);
-console.log(`Elements: ${liTechnologiesArr.length}`);
+console.log(`Elements: ${liArr[2].length}`);
