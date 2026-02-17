830fc0070a02295413ab39b24c3a7601873c82f0
Ign1s123 <genamisinskij@gmail.com>
2026-02-17 22:52:46 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(`.login-form`)
+// const submitBtn = form.querySelector(`button`)
 
+form.addEventListener(`submit`, (e) => {
+    e.preventDefault();
+
+    if(form.elements.email.value.trim() === `` || form.elements.password.value.trim() === ``){
+        alert('All form fields must be filled in');
+    }
+
+    else{
+        const value = {
+            email: form.elements.email.value.trim(),
+            password: form.elements.password.value.trim()
+        }
+        console.log(value);
+        form.reset();
+    }
+})
