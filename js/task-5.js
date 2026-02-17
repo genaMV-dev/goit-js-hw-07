a91a6fa442b9cfce8fa51d2cf4f1413e9ec9ec6c
Ign1s123 <genamisinskij@gmail.com>
2026-02-17 23:08:29 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,12 @@
+const button = document.querySelector(`.change-color`);
+const body = document.querySelector(`body`);
+
+button.addEventListener(`click`, () => {
+  body.style.backgroundColor = getRandomHexColor();
+})
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
